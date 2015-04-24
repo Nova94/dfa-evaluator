790da9391984603a5e296abc4fce79d4220483ec
Lisa Gray <graylisa94@gmail.com>
2015-04-24 14:35:28 -0700

    managed JSON and other IO things

--- DFAJSON.hs
+++ DFAJSON.hs
@@ -1,6 +1,11 @@
---TODO: convert from Delta?
+{- Author - Lisa Gray
+   Version - 1.0
+   Date - 4/24/15
+-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+
+module DFAJSON where
 
 import qualified Data.ByteString.Lazy as B
 import Control.Applicative
@@ -10,21 +15,14 @@ import Data.Maybe as Maybe
 
 type Dtuple = ((String, Char), String)
 
-data Delta =
-    Delta ((String, Char), String)
-      deriving (Read,Show)
-
-readDelta :: String -> Maybe Delta
-readDelta s = do
+toDelta :: String -> Maybe Dtuple
+toDelta s = do
     b <- return $ read s
     return b
 
---fromDelta :: (Delta a, Dtuple b) => a -> b
-
-d = "((\"q0\",\'1\'), \"q1\")"
-
-x :: Delta
-x = Delta (("q0", '2'), "q1")
+-- y <- getJSON
+-- decode y :: Maybe DfaJSON
+--fromJust y
 
 data DfaJSON =
   DfaJSON { states :: [String]
@@ -43,13 +41,12 @@ jsonFile = "dfa.json"
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
-maybeReadTup :: String -> Maybe (Int, Char)
-maybeReadTup s = do
-    [(n, [c])] <- return $ reads s
-    return (n, c)
+readJSON = do
+    y <- getJSON
+    return (fromJust(decode y :: Maybe DfaJSON))
 
-{- maybeReadMap :: String -> Maybe ((a, b), a)
-maybeReadMap s = do
-    [((is, c), ns)] <- return $ reads s
-    return ((is, c), ns)
--}
+getDeltaMapJSON :: [String] -> [Dtuple]
+getDeltaMapJSON [] = []
+getDeltaMapJSON [x] = [(fromJust (toDelta x))]
+getDeltaMapJSON (x:xs) =
+    (fromJust (toDelta x)) : getDeltaMapJSON xs
--- dfa.hs
+++ dfa.hs
@@ -1,35 +1,61 @@
+{- Author - Lisa Gray
+   Version - 1.0
+   Date - 4/24/15
+-}
+
 import qualified Data.Map as Map
 import qualified Data.Maybe as Maybe
+import DFAJSON
 
 --(set of States, Sigma, delta function, start state, isFinal)
 type DFA = ([[Char]], [Char], (([Char], Char) -> [Char]), [Char], ([Char] -> [[Char]] -> Bool))
 
+--sets deltaMapping to an empty map
+deltaMapping :: Map.Map ([Char], Char) [Char]
+deltaMapping = Map.empty
+accs = []
+
+--Checks whether current state is an accept state
+inFinal :: Eq a => a -> [a] -> Bool
+inFinal st l = st `elem` l
+
+main = do
+    dfa <- readJSON
+    let q = states dfa --get Q from JSON
+        s = sigma dfa -- get Sigma from JSON
+        dmap = deltamap dfa --Get mapping of states from JSON
+        ss = startstate dfa --get start state from dfa
+        accs = acceptstates dfa --get list of Accept states
+        --get mapping from json and store in delta mapping
+        deltaMapping = Map.fromList (getDeltaMapJSON dmap)
+        delta (q, x) = Maybe.fromJust ( Map.lookup (q,x) deltaMapping )
+        --I had to define the evaluateDFA here because it didn't like it elsewhere
+        evaluateDFA :: DFA -> [Char] -> Bool
+        evaluateDFA (qset, sigma, d, ss, inFinal) [] = inFinal ss accs
+        evaluateDFA (qset, sigma, d, ss, inFinal) [x] = inFinal (d (ss, x)) accs
+        evaluateDFA (qset, sigma, d, ss, inFinal) (x:xs) =
+            evaluateDFA (qset, sigma, d, ns, inFinal) xs
+            where ns = d (ss, x)
+    --empty Sigma = empty set language otherwise get string
+    if (null s)
+        then print True
+        else do
+            x <- getLine
+            print deltaMapping
+            print accs
+            print (evaluateDFA (q,s,delta,ss,inFinal) x)
+
+{- Original declarations to test evaluation
 --myDFA
 q = ["q0", "q1", "q2", "q3"]
 s = ['0' , '1']
-deltaMapping = Map.fromList [ (("q0", '0'), "q0")
-                            , (("q0", '1'), "q1")
-                            {-, (("q1", '0'), "q3")
-                            , (("q1", '1'), "q2")
-                            , (("q2", '0'), "q2")
-                            , (("q2", '1'), "q2")
-                            , (("q3", '0'), "q3")
-                            , (("q3", '1'), "q3")]-}
-
-                            , (("q1", '0'), "q0")
-                            , (("q1", '1'), "q2")
-                            , (("q2", '0'), "q2")
-                            , (("q2", '1'), "q1") ]
-
-accs = ["q2"]
 
 m :: DFA --[Char] Char
 m = (q,s, delta, "q0", inFinal)
 
 --make delta function
+delta :: ([Char], Char) -> [Char]
 delta (q, x) = Maybe.fromJust ( Map.lookup (q,x) deltaMapping )
-
-inFinal st l = st `elem` l
 
 
 evaluateDFA :: DFA -> [Char] -> Bool
@@ -38,3 +64,4 @@ evaluateDFA (qset, sigma, d, ss, inFinal) [x] = inFinal (d (ss, x)) accs
 evaluateDFA (qset, sigma, d, ss, inFinal) (x:xs) =
     evaluateDFA (qset, sigma, d, ns, inFinal) xs
     where ns = d (ss, x)
+-}
